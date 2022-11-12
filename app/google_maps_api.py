e4121613adea4c23a7da182bccef4157cdf9bbc4
斉藤義弥 <saitouyoshiya@saitouyoshiwatarunoMacBook-Pro.local>
2022-11-12 12:58:45 +0900

    nine

--- app/google_maps_api.py
+++ app/google_maps_api.py
@@ -12,19 +12,3 @@ def geocode(address):
        return lat, lng
    except:
        return None, None
-
-# 住所リストを読み込む
-# 住所リストには　address というカラムが必須
-filepaths = glob('address.xlsx')
-filepath = filepaths[0]
-df = pd.read_excel(filepath)
-
-# 緯度経度データの取得
-result = [] # 取得した緯度経度を入れておくリスト
-for ad in tqdm(df['address']):
-   result.append(geocode(ad))
-
-# 取得したデータの保存
-_df = pd.DataFrame(result, columns=['altitude', 'longitude'])
-df = df.join(_df)
-df.to_excel('address_with_altlong.xlsx', index=False)
